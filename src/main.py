ff238cde2482bf5309cce56b3096da8a2ff518cd
arseniiarsenii <arseniivelichko2@gmail.com>
2022-10-06 19:39:35 +0300

    feat: Add .env files parsing

--- src/main.py
+++ src/main.py
@@ -1,15 +1,28 @@
 import asyncio
-from pprint import pprint
+from pathlib import Path
+from time import time
 
-from wiki_deprecation_notifier._dependency_mapping import get_dependency_map
+import dotenv
+from loguru import logger
+
+from wiki_deprecation_notifier import run_inspection
+
+dotenv_file = Path(".env")
+if dotenv_file.exists():
+    dotenv.load_dotenv(dotenv_file.absolute(), override=True)
+
+
+async def main() -> None:
+    t0 = time()
+    await run_inspection()
+    logger.debug(f"Run time: {time() - t0}s")
+
 
 if __name__ == "__main__":
-    res = asyncio.run(get_dependency_map())
-    pprint(res)  # noqa: T203
+    asyncio.run(main())
 
 # TODO: Add docker-compose
 # TODO: Add logging
-# TODO: Add conflict detection
 # TODO: Add Sqlite DB
 # TODO: Implement Telegram bot functions
 # TODO: Implement issue creation
