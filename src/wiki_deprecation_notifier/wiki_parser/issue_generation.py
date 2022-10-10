d2e1325645958f6d769e9de4ecdc0bd0f75f303d
arseniiarsenii <arseniivelichko2@gmail.com>
2022-10-10 14:47:03 +0300

    fix: Modified Issue structure to satisfy contribution guidelines

--- src/wiki_deprecation_notifier/wiki_parser/issue_generation.py
+++ src/wiki_deprecation_notifier/wiki_parser/issue_generation.py
@@ -12,13 +12,15 @@ if TYPE_CHECKING:
 def create_issue(conflict: DeprecationConflict) -> Issue:
     issue_title = "".join(  # noqa: ECE001
         (
-            f"Article {conflict.article.name} is deprecated due to",
-            f'a release "{conflict.dependency.latest_release.name}"',
+            "[Issue]: ",
+            f"Article {conflict.article.name} is deprecated due to ",
+            f'a release "{conflict.dependency.latest_release.name}" ',
             f'in "{conflict.dependency.name}".',
         )
     )
     issue_body = "".join(  # noqa: ECE001
         (
+            "### Issue description\n\n",
             f'Article ["{conflict.article.name}"]({conflict.article.url}) ',
             "has been automatically marked as deprecated due to ",
             f'a recent release ["{conflict.dependency.latest_release.name}"]({conflict.dependency.latest_release.url})',
@@ -28,6 +30,8 @@ def create_issue(conflict: DeprecationConflict) -> Issue:
             "\n".join(f"- {c.handle}" for c in conflict.article.contributors),
             "\n\n",
             f"Deprecation reference id: {conflict.conflict_hash}",
+            "\n\n### Doc Page\n\n",
+            conflict.article.name,
         )
     )
     return Issue(
